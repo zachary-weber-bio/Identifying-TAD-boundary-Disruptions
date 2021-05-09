060c386f8957d523593bbe8c21ac4b2295c08ef9
H@rri2710z! <zach.weber.813@gmail.com>
2021-05-09 15:13:00 -0500

    added scripts directory

---
+++ scripts/runManta.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# File Name: runManta.sh
+# Created By: ZW
+# Created On: 2021-05-09
+# Purpose: runs the Illumina MANTA SV caller. on a WGS sample
+
+# Usage message for the runMANTA.sh script
+usage () {
+    echo "Usage: runManta.sh [-b <BAM file>] [-i <BAI file>]
+                    [-s <Sample ID>] [-r <Ref. FASTA>] [-c <Call Regions>]
+                    [-t <Temp Directory>] [-o <Output Directory>]
+                    [-j <Jobs>] [-g <Memory(GB)>]" 1>&2;
+    exit 1;
+}
+
+# getopt setup for named arguments
+while getopts ":b:i:s:r:o" x; do
+    case "${x}" in
+        b) b=${OPTARG} ;;
+        i) i=${OPTARG} ;;
+        s) s=${OPTARG} ;;
+        r) r=${OPTARG} ;;
+        c) c=${OPTARG} ;;
+        t) t=${OPTARG} ;;
+        o) o=${OPTARG} ;;
+        j) j=${OPTARG} ;;
+        g) g=${OPTARG} ;;
+        *) usage ;;
+    esac
+done
+
+# ----- Run MANTA ------
+# switch to temp execution directory and
+# run the MANTA configuration step
+mkdir ${t} && cd ${t}
+./configManta.py \
+    --bam ${b} \
+    --referenceFasta ${r} \
+    --callRegions ${c}
+
+# run MANTA, then collect and rename results
+./runWorkflow.py -j ${j} -g ${g}
+mv results/variants/diploidSV.vcf.gz results/variants/${s}_diploidSV.vcf.gz
+mv results/variants/diploidSV.vcf.gz.tbi results/variants/${s}_diploidSV.vcf.gz.tbi
+
+cp results/variants/${s}_diploidSV.vcf.gz ${o}
+cp results/variants/${s}_diploidSV.vcf.gz.tbi ${o}
